ec933ead5880d170e50fff65f1c814cb26225cf6
salberico <sam.alberico@gmail.com>
2017-01-06 22:19:20 -0500

    resolved conflicts

---
+++ Assets/Resources/Tile/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Tile test = new Tile (-0.5f, 0.5f, 1, true, false);
+		test.setNumber (1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
